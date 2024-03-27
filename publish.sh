34f9c16db71e3ce0a129faac085a142e7e18310d
Marcellus Siegburg <marcellus.siegburg@uni-due.de>
2024-03-27 13:35:23 +0100

    fix deployment script

--- publish.sh
+++ publish.sh
@@ -10,18 +10,22 @@ PORT=22
 KET=codeworld.keter
 TARGET_FOLDER=/opt/codeworld
 
+LC_ALL=C
+
 TIME=`date +"%Y-%m-%d-%H.%M.%S"`
 if ! [[ -v REMOTE_DIR ]]
 then
   REMOTE_DIR=${TARGET_FOLDER}${TIME}
 fi
 
+ssh ${JUMP_HOST:+-J} ${JUMP_HOST:+"${JUMP_HOST}"}\
+  -p "${PORT}" "${USER}@${SERVER}" -C "mkdir -p ${REMOTE_DIR}"
 
 scp ${JUMP_HOST:+-J} ${JUMP_HOST:+"${JUMP_HOST}"}\
-  -P "${PORT}" "$KET" "${USER}@${SERVER}:${REMOTE_DIR}"
+  -P "${PORT}" "$KET" "${USER}@${SERVER}:${REMOTE_DIR}/$KET"
 
 ssh ${JUMP_HOST:+-J} ${JUMP_HOST:+"${JUMP_HOST}"}\
-  -P "${PORT}" "${USER}@${SERVER}" -C "mkdir -p ${REMOTE_DIR}/codeworld\
+  -p "${PORT}" "${USER}@${SERVER}" -C "mkdir -p ${REMOTE_DIR}/codeworld\
   && cd ${REMOTE_DIR}/codeworld\
   && tar xf ../$KET\
   && rm ${REMOTE_DIR}/$KET\
