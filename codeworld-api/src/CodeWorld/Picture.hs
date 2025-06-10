489f8fd27a9b83febd7e541f7efba145470b9e85
nimec01 <24428341+nimec01@users.noreply.github.com>
2025-06-10 13:19:59 +0200

    wording

--- codeworld-api/src/CodeWorld/Picture.hs
+++ codeworld-api/src/CodeWorld/Picture.hs
@@ -267,11 +267,11 @@ circle = Circle (getDebugSrcLoc callStack)
 
 -- | A thick circle, with this line width and radius
 --
--- The line width must not be greater than the diameter.
+-- The line width must not be greater than its diameter.
 thickCircle :: HasCallStack => Double -> Double -> Picture
 thickCircle a r 
   | a <= 2 * r = ThickCircle (getDebugSrcLoc callStack) a r
-  | otherwise = error "The line width of a thickCircle must not be greater than the diameter."
+  | otherwise = error "The line width of a thickCircle must not be greater than its diameter."
 
 -- | A thin arc, starting and ending at these angles, with this radius
 --
