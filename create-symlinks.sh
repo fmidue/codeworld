2455f3bfe45571e8d8de424ad133601f8b3b2ea2
nimec01 <24428341+nimec01@users.noreply.github.com>
2025-07-30 13:48:34 +0200

    no need to rm in create-symlinks

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -2,8 +2,6 @@
 
 set -u
 
-rm -rf web/codeworld-base.txt web/mirrored web/js/codemirror-compressed.js web/SourceCodePro web/js/diff.min.js web/js/codemirror-buttons/buttons.js web/doc web/doc-haskell web/index.html web/gallery.html web/gallery-icfp17.html web/blockly web/js/blocks_lib.js web/js/blocks_out.js web/js/blocks_rts.js web/js/blocks_ain.js web/css/ambiance.css web/css/codemirror.css web/css/lint.css web/css/show-hint.css web/ims web/help/ims
-
 cp codeworld-base/dist/doc/html/codeworld-base/codeworld-base.txt web/codeworld-base.txt
 grep -r -s -h 'pattern\s*[A-Za-z_0-9]*\s*::.*' /home/codeworld/codeworld-base >> $HOME/web/codeworld-base.txt
 
